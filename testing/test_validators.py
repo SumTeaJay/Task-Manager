c68f627c0802d2a270f770c9adaa3258cc497d4a
Sum Tea Jay <112896792+SumTeaJay@users.noreply.github.com>
2026-08-18 16:29:43 +0300

    Тесты для валидаторов и меню

---
+++ testing/test_validators.py
@@ -0,0 +1,41 @@
+import pytest
+
+from app.validators import validate_name, validate_task
+
+###Валидация имени задачи###
+@pytest.mark.parametrize(
+    "name",
+    [
+        "n",
+        "h" * 150,
+    ]
+)
+def test_validate_name_valid(name):
+    result = validate_name(name)
+
+    assert result is None
+
+@pytest.mark.parametrize(
+    "name, message",
+    [
+        ("", "Отсутствует имя"),
+        ("h" * 151, "Название задачи слишком длинное: 151 > 150"),
+    ]
+)
+def test_validate_name_invalid(name, message):
+    with pytest.raises(ValueError, match=message):
+        validate_name(name)
+
+###Валидация целой задачи###
+@pytest.mark.parametrize(
+        "task",
+        [
+            ({"user_name": "sum_tea_jay", "status": "выполнено", "deadline": "12.09.2007"}),
+            ({"user_name": "sum_tea_jay", "name": "name", "deadline": "12.09.2007"}),
+            ({"status": "не выполнено", "name": "name", "deadline": "12.09.2007"}),
+            ({"user_name": "user", "status": "не выполнено", "name": "name"})
+        ]
+)
+def test_validate_task_headers(task):
+    with pytest.raises(KeyError):
+        validate_task(task)
