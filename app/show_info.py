72b051a49e5eac68bec3e67db1121e681a6e709f
Sum Tea Jay <112896792+SumTeaJay@users.noreply.github.com>
2026-08-22 20:21:24 +0300

    Подсветка цифр, жирный текст и точки с запятыми

--- app/show_info.py
+++ app/show_info.py
@@ -6,7 +6,7 @@ def print_tasks(user: str, tasks: list[dict[str, str]], status=None) -> None:
         tasks = list(filter(lambda task: task["status"] == status, tasks))
     if tasks:
         for index, task in enumerate(tasks, 1):
-            type_text(f"{index}) {task["name"]}\nИдентификатор: {task["id"]}\nСтатус: {task["status"]}\nДедлайн: {task["deadline"]}")
+            type_text(f"{index}) {task["name"]};\nИдентификатор: {task["id"]};\nСтатус: {task["status"]};\nДедлайн: {task["deadline"]}.")
     else:
         type_text("У вас нет задач по заданному фильтру.")
 
--- app/ui.py
+++ app/ui.py
@@ -1,8 +1,12 @@
 import time
+from rich.console import Console
+from rich.theme import Theme
 
 def type_text(text, delay=0.03):
+    custom_theme = Theme({"repr.number": "#62a874"})
+    bold_concole = Console(style="bold", theme=custom_theme)
     for symbol in text:
-        print(symbol, end="", flush=True)
+        bold_concole.print(symbol, end="")
         time.sleep(delay)
     time.sleep(delay * 10)
     print()
